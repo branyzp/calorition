d775c54ecd7ee6564d812e15b6359f543dedaed4
Brandon Yeo <branyzp@gmail.com>
2022-05-29 15:36:25 +0800

    initial wireframe

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBar = () => {
+	return (
+		<form>
+			<input type="text" placeholder="Search a food!" />
+			<button>Submit</button>
+		</form>
+	);
+};
+
+export default SearchBar;
