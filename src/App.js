71e0757c97039f187516939fa31907a064568443
Brandon <branyzp@gmail.com>
2022-05-30 00:28:23 +0800

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import SearchBar from './Components/SearchBar';
 import Home from './Pages/Home';
+import About from './Pages/About';
+import Header from './Components/Header';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+
+const API_ID = process.env.REACT_APP_API_ID;
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+console.log('API_ID is ' + API_ID);
+console.log('API Key is ' + API_KEY);
 
 function App() {
-	fetch();
-
 	return (
 		<>
-			<Home />
-			<SearchBar />;
+			<BrowserRouter>
+				<Navbar />
+
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="about" element={<About />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Button } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 
 const SearchBar = () => {
 	return (
 		<form>
 			<input type="text" placeholder="Search a food!" />
-			<button>Submit</button>
+			<Button
+				endIcon={<SendIcon />}
+				onClick={() => alert('Yello')}
+				variant="outlined"
+			>
+				Submit
+			</Button>
 		</form>
 	);
 };
